a1bffdc2e8575eb8708b56825c8fb0778c64b87c
Qu Zhun <cueroqu@gmail.com>
2013-10-17 08:49:00 +0800

    add active page

--- thuwine/core.php
+++ thuwine/core.php
@@ -2,6 +2,7 @@
     require_once 'system/templater.php';
     $content = new Templater('core.content.php');
     $page = new Templater('templates/main.tpl.php');
+    $page->set('active_page', 'core');
     $page->set('content', $content->parse());
     $page->publish();
 ?>
