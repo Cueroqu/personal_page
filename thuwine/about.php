d5a9e84dca1574ff8100f10a8095f5521623b029
Qu Zhun <cueroqu@gmail.com>
2013-11-07 14:12:58 +0800

    add logo to main page

--- thuwine/about.php
+++ thuwine/about.php
@@ -2,5 +2,6 @@
     <br/>
     <h1 style="text-align:center; font-size:22px;">Welcome to Tsinghua University Wine Association</h1>
     <br/>
+    <img src="images/grapevineLogo.png" style="border:0px; width:50%; display:table-cell; margin-left:auto; margin-right:auto; margin-top:30px; margin-bottom:30px; " />
     <p style="margin-left: 20%; margin-right: 20%; text-align:center; font-size:14px; font-family: Georgia;">Tsinghua University's premier wine society organizes frequent tastings for our members. Both introduction to wines and subjects on the regions or estates have been hosted.
 </div>
